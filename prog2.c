625fd8f62eb0570a6305b0f69df7ca15d06ea503
Alexey Akulenko <akulenko21.02.01@gmail.com>
2024-06-20 14:22:38 +0300

    Создает файл .csv, с разделитемя - запятыми, а так же выводит частоту, заданную в .cfg файле. После вручную преобразовал .csv в xlsx и вставил диаграммы

--- prog2.c
+++ prog2.c
@@ -1,14 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 
 int main() {
-    char* filename = "prog1.cfg";
-    char buffer[256];
-    FILE* fp = fopen(filename, "r");
-    if (fp) {
-        while ((fgets(buffer, 256, fp)) != NULL)
-        printf("%s", buffer);
-    fclose(fp);
+	// Локализация русского языка
+	setlocale(LC_ALL, "RUS");
+
+	// Создание и открытие файлов .csv, .cfg и .dat
+	char buffer[1024];
+	FILE* datFile = fopen("prog1.dat", "r");
+	FILE* csvFile = fopen("prog2.csv", "w");
+	FILE* cfgFile = fopen("prog1.cfg", "r");
+
+	// Проверка успешного открытия файлов
+	if (datFile == NULL || csvFile == NULL || cfgFile == NULL) {
+		printf("Ошибка открытия файла.\n");
+		return 1;
+	}
+
+	// Количество сигналов и частота сети
+	int numChan, freq;
+
+	fgets(buffer, sizeof(buffer), cfgFile); // Первая строка
+	fgets(buffer, sizeof(buffer), cfgFile); // Вторая строка, содержащая количество сигналов
+	sscanf(buffer, "%d", &numChan);
+	
+	// Пропуск строк, содержащих информацию о сигналах
+	for (int i = 0; i < numChan; i ++) {
+		fgets(buffer, sizeof(buffer), cfgFile);
+	} 
+	// Чтение строки содержащей частоту
+	fgets(buffer, sizeof(buffer), cfgFile); 
+	sscanf(buffer, "%d", &freq);
+	
+	// Запись данных в файл .csv 
+	if (datFile) {
+		while ((fgets(buffer, sizeof(buffer), datFile)) != NULL) {
+			fprintf(csvFile, "%s", buffer);
+		}
+	}
+        
+        // Закрытие файлов
+	fclose(datFile);
+	fclose(csvFile);
+	fclose(cfgFile);
+
+	printf("Файл .cfg успешно создан.\n");
+	printf("Частота сети: %d\n", freq);
+
+	return 0;
+
 }
-}
+
